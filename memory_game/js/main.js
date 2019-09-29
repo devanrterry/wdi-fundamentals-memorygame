574bc68cb3e09157ce5895c9beca521f822558d4
Devan Terry <devanterry@Devans-MacBook-Pro.local>
2019-09-28 22:39:20 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push("cardOne");
+
+console.log("User flipped queen")
+
+var cardTwo = cards[2];
+
+cardsInPlay.push("cardTwo")
+
+console.log("User flipped king")
+
+if (length.cardsInPlay === 2){
+
+}	else if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+}
+	else {
+	alert("Sorry, try again.");
+	}
